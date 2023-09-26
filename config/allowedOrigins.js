b5789e684371581adaf44eba51babaf3ef11991f
AlphaOmegaPepega <slagernauk@gmail.com>
2023-09-26 19:08:21 +0300

    clock

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,11 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://productizemanager.vercel.app/',     
-    'https://www.https://productizemanager.vercel.app/',
+    'https://productizemanager.vercel.app',
+    
+
+    
+    'https://www.productizemanager.vercel.app',
+
 ]
 
 module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -56,4 +56,3 @@ mongoose.connection.on('error', err => {
     console.log(err)
     logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
-module.exports=app
